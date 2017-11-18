c6cd61d56325b5af0a55475f37d3d784c50ceea8
Brar Piening <brar@gmx.de>
2017-11-18 07:58:28 +0100

    Add initial API

---
+++ src/AuthServer/Config.cs
@@ -0,0 +1,57 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace AuthServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource
+                {
+                    Name = "NeoKiss",
+                    DisplayName = "The NEO-KISS API",
+
+                    Scopes =
+                    {
+                        new Scope()
+                        {
+                            Name = "NeoKiss.FullAccess",
+                            DisplayName = "Full access to the NEO-KISS API"
+                        },
+                        new Scope()
+                        {
+                            Name = "NeoKiss.ReadOnly",
+                            DisplayName = "Read only access to the NEO-KISS API"
+                        }
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "NeoKiss.ReadOnly" }
+                }
+            };
+        }
+    }
+}
--- src/AuthServer/Startup.cs
+++ src/AuthServer/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AuthServer
@@ -15,8 +10,11 @@ namespace AuthServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // configure identity server with in-memory stores, keys, clients and resources
             services.AddIdentityServer()
-                .AddDeveloperSigningCredential();
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
